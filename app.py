ab9844ca63fd3513bfad29044667bd31315fdb5c
Ken Hsu <92627137+hsue2000@users.noreply.github.com>
2025-08-19 23:21:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -184,22 +184,12 @@ SECRET = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 API_TOKEN = os.getenv("API_TOKEN")
 API_BASE_URL = os.getenv("API_BASE_URL")
 
-
-# 可使用的 LINE 使用者 ID 列表（White List）
-whitelist = {
-    "Ub48499f073b0bd08e280ef8259978933",  # 用戶A
-    "Uyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy",  # 用戶B
-    # 請將你自己的 LINE ID 也加入
-}
-
-"""
 # 從 Vercel 的環境變數讀取
 whitelist_str = os.getenv("LINE_WHITELIST", "")
 
 # 轉成 set（自動去除空白）
 whitelist = {uid.strip() for uid in whitelist_str.split(",") if uid.strip()}
 # print(whitelist)
-"""
 
 CHANNEL_ACCESS_TOKEN = (os.getenv("LINE_CHANNEL_ACCESS_TOKEN") or "").strip().strip('"')
 CHANNEL_SECRET = (os.getenv("LINE_CHANNEL_SECRET") or "").strip().strip('"')
@@ -1225,3 +1215,4 @@ def handle_message(event):
 
 if __name__ == "__main__":
     app.run(port=5000)
+
