f857335569bfeab46fc9fe82432da1aa7a708570
Ken Hsu <92627137+hsue2000@users.noreply.github.com>
2025-08-30 15:33:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -202,20 +202,12 @@ API_BASE_URL = os.getenv("API_BASE_URL")
 
 
 # 可使用的 LINE 使用者 ID 列表（White List）
-whitelist = {
-    "Ub48499f073b0bd08e280ef8259978933",  # 用戶A
-    "Uyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy",  # 用戶B
-    # 請將你自己的 LINE ID 也加入
-}
-
-"""
 # 從 Vercel 的環境變數讀取
 whitelist_str = os.getenv("LINE_WHITELIST", "")
 
 # 轉成 set（自動去除空白）
 whitelist = {uid.strip() for uid in whitelist_str.split(",") if uid.strip()}
 # print(whitelist)
-"""
 
 CHANNEL_ACCESS_TOKEN = (os.getenv("LINE_CHANNEL_ACCESS_TOKEN") or "").strip().strip('"')
 CHANNEL_SECRET = (os.getenv("LINE_CHANNEL_SECRET") or "").strip().strip('"')
@@ -263,10 +255,10 @@ response = requests.get(image_url)
 image_data = BytesIO(response.content)
 
 # 上傳圖片
-# line_bot_api.set_rich_menu_image(rich_menu_id, "image/png", image_data)
+line_bot_api.set_rich_menu_image(rich_menu_id, "image/png", image_data)
 
 # 設為預設選單
-# line_bot_api.set_default_rich_menu(rich_menu_id)
+line_bot_api.set_default_rich_menu(rich_menu_id)
 
 ######################################################################
 
@@ -1259,3 +1251,4 @@ def handle_message(event):
 
 if __name__ == "__main__":
     app.run(port=5000)
+
